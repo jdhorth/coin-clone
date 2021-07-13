14540b4172bc0f9eb5610fd78f2a559d1e49f252
Justin Horth <jdhorth@gmail.com>
2021-07-13 13:26:55 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -23,65 +23,67 @@ export default function Home(props) {
       .format(number);
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>CryptoBitPunk Coin Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>CryptoBitPunk Coin Tracker</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <h1>CryptoBitpunk Coin Tracker</h1>
-      <h6>Top 100 powered by CoinGecko API, Next js, Vercel, developed by non-fungi.com</h6>
+        <h1>CryptoBitpunk Coin Tracker</h1>
+        <h6>Top 100 powered by CoinGecko API, Next js, Vercel, developed by non-fungi.com</h6>
 
-      <table className='table bg-dark text-info'>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>24hr +/- %</th>
-            <th>24hr +/- $</th>
-            <th>Current Price</th>
-            <th>All Time High</th>
-            <th>Market Cap</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(coin => (
-            <tr key={coin.id}>
-              <td>
-                <img
-                  src={coin.image}
-                  style={{ width: 25, height: 25, marginRight: 10 }}
-                />
-                {coin.symbol.toUpperCase()}</td>
-              <td>
-                <span
-                  className={coin.price_change_percentage_24h > 0 ? (
-                    'text-success'
-                  ) : 'text-danger'}
-                >
-                  {formatPercent(coin.price_change_percentage_24h)}
-                </span>
-              </td>
-              <td>
-                <span
-                  className={coin.price_change_24h > 0 ? (
-                    'text-success'
-                  ) : 'text-danger'}
-                >
-                  {formatDollar(coin.price_change_24h)}
-                </span>
-              </td>
-              <td>{formatDollar(coin.current_price, 20)}</td>
-              <td>{formatDollar(coin.ath, 12)}</td>
-              <td>{formatDollar(coin.market_cap, 12)}</td>
+        <table className='table bg-dark text-info'>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>24hr +/- %</th>
+              <th>24hr +/- $</th>
+              <th>Current Price</th>
+              <th>All Time High</th>
+              <th>Market Cap</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map(coin => (
+              <tr key={coin.id}>
+                <td>
+                  <img
+                    src={coin.image}
+                    style={{ width: 25, height: 25, marginRight: 10 }}
+                  />
+                  {coin.symbol.toUpperCase()}</td>
+                <td>
+                  <span
+                    className={coin.price_change_percentage_24h > 0 ? (
+                      'text-success'
+                    ) : 'text-danger'}
+                  >
+                    {formatPercent(coin.price_change_percentage_24h)}
+                  </span>
+                </td>
+                <td>
+                  <span
+                    className={coin.price_change_24h > 0 ? (
+                      'text-success'
+                    ) : 'text-danger'}
+                  >
+                    {formatDollar(coin.price_change_24h)}
+                  </span>
+                </td>
+                <td>{formatDollar(coin.current_price, 20)}</td>
+                <td>{formatDollar(coin.ath, 12)}</td>
+                <td>{formatDollar(coin.market_cap, 12)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
-      <h1>CryptoBitpunk</h1>
-      <h6>Realtime updated stats powered by CoinGecko API, Next js, Vercel, developed by non-fungi.com</h6>
+        <h1>CryptoBitpunk</h1>
+        <h6>Realtime updated stats powered by CoinGecko API, Next js, Vercel, developed by non-fungi.com</h6>
 
-    </div>
+      </div>
+    </>
   )
 }
 
